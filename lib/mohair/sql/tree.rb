d064cc27bb2e8e7116557e57b9b74f050f725dfc
UENISHI Kota <kuenishi@gmail.com>
2013-05-23 00:13:14 +0900

    files

---
+++ lib/mohair/sql/tree.rb
@@ -0,0 +1,29 @@
+## syntax tree
+
+module Mohair
+
+  class Select
+  end
+
+  class Insert
+  end
+
+  class Update
+  end
+
+  class Delete
+  end
+
+  class From
+  end
+
+  class Where
+  end
+
+  class Order
+  end
+
+  class Group
+  end
+
+end
---
+++ test/plugin/selector.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+
+class SelectorTest < MiniTest::Unit::TestCase
+  def setup
+  end
+  def teardown
+  end
+  def test_one
+  end
+end
